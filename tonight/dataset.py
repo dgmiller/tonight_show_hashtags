8450fcd584898991a64fc04599c63837424396ce
Kolten Pearson <koltenpearson@gmail.com>
2015-12-15 17:29:27 -0700

    reorganizing things for the website, and made the beginnings of a dataset object

---
+++ tonight/dataset.py
@@ -0,0 +1,77 @@
+import sys
+import os
+import fileutil
+import re
+
+DATA_DIR = os.path.join('..', 'data')
+RAW_DIR = os.path.join(DATA_DIR, 'raw')
+FILTER_DIR = os.path.join(DATA_DIR, 'filter')
+
+
+
+class dataset :
+
+    #----static initializtion stuff ---------------------------------------------
+    fileutil.create_folders([RAW_DIR, FILTER_DIR]) #not sure how good of an idea this is, but it should work
+
+
+    #----static methods---------------------------------------
+
+    ##this method will return the name passed in to it, or else
+    #it will return a unique name that is close
+    @staticmethod
+    def get_unique_name(name) : #TODO I should probably throw a mutex on this to make it threadsafe
+
+        used_names = os.listdir(RAW_DIR)
+
+        if name in used_names :
+
+            count = 2
+            while ((name + str(count)) in used_names) :
+                count += 1
+
+            name += str(count)
+
+        open(os.path.join(RAW_DIR, name), 'x').close() #create the file immediately
+        return name
+
+    ##creates a new dataset
+    # will ensure that the name is unique, and based on the hashtag
+    # \returns a tuple, the first item is a reference to the object that was made
+    #           the second is a method, it takes a list of strings and will append them to the raw data
+    #           it is only to be used before calling any other methods, that behavior is not enforced at the moment
+    @staticmethod
+    def make_new(hashtag) :
+        result = dataset()
+        result.hashtag= hashtag
+        if hashtag[0] == '#' :  #get rid of # in hashtag if it is there
+            result.hashtag = hashtag[1:]
+
+        result.name = dataset.get_unique_name(hashtag)
+
+        def inject_raw_data(lines) :
+            fileutil.append_file(os.path.join(RAW_DIR, result.name), lines)
+
+        return (result, inject_raw_data)
+
+
+    ##this will load an already existing dataset
+    @staticmethod
+    def load(name) :
+        self.name = name
+
+        raw_data = fileutil.read_file(os.path.join(RAW_DIR, self.name)) 
+
+        i = 0
+        running = ['', 0]
+        while (running[1] < 5) : #quick and dirty algorithm to detect hashtag
+            #TODO come up with a better solution, this one requires the dataset to be fairly large to work, and is fairly ineffecient anyway
+            tag = re.match(r'^.*(#\S+).*$', raw_data[i]).group(1)
+
+            if (tag == running[0]) :
+                running[1] += 1
+            else :
+                running[0] = tag
+                running[1] = 0
+
+        self.hashtag = running[0][1:]
---
+++ tonight/fileutil.py
@@ -0,0 +1,62 @@
+##this file contains some helpful functions for io and other file manipulations
+#its main purpose is to help us be threadsafe
+
+import fcntl
+import os
+
+##this will ensure that the given directories exist, 
+#if they do not already exist it will create them
+#\param folder, the folder to create if it does not exist, if passed a list it will assume it is a list of folders to create
+def create_folders(folder) :
+
+    if not isinstance(folder, (list, tuple)) :
+        folder = [folder]
+
+    for f in folder :
+        if not os.path.exists(f) :
+            os.makedirs(f)
+
+## thread safe writing of lines to a file
+# will not return until written
+# will always overwrite whatever is already there
+# \param each line goes on a separate line, new lines will be added to the ended before written
+def write_file(filename, lines) :
+
+    f = open (filename, 'w')
+    fcntl.lockf(f, fcntl.LOCK_EX) #get the lock for the file
+
+    f.writelines([l + '\n' for l in lines])
+    f.flush()
+
+    fcntl.lockf(f, fcntl.LOCK_UN)
+    f.close()
+
+## thread safe appending of lines to a file
+# will not return until written
+# \param each line goes on a separate line, new lines will be added to the ended before written
+def append_file(filename, lines) :
+
+    f = open (filename, 'a')
+    fcntl.lockf(f, fcntl.LOCK_EX) #get the lock for the file
+
+    f.writelines([l + '\n' for l in lines])
+    f.flush()
+
+    fcntl.lockf(f, fcntl.LOCK_UN)
+    f.close()
+
+
+## thread safe reading of a file
+# \return a list of strings, each string one line, whitespace will be stripped from either side
+def read_file(filename) :
+
+    f.open(filename)
+    fcntl.lockf(f, fcntl.LOCK_SH)
+
+    lines = f.readlines()
+
+    fcntl.lockf(f, fcntl.LOCK_UN)
+    f.close()
+
+    return [l.strip() for l in lines]
+
