4f832e75cd3168deaab673cc2e0622cd4c6b5211
Kolten Pearson <koltenpearson@gmail.com>
2015-12-31 16:03:47 -0700

    okay now there is a way to view the data we collect, and see some results, very rough though, we still have a long ways to go

--- website/website.py
+++ website/website.py
@@ -7,6 +7,7 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),'..' ))
 from tonight import streamer
+from tonight import dataset
 
 stream = streamer.get_driver()
 
@@ -35,6 +36,24 @@ def scraper() :
 @app.route('/') 
 def main_page():
     return redirect(url_for('login'))
+
+@app.route('/viewer', methods=['GET', 'POST'])  #TODO this is tightly coupled with the current setup of dataset
+def data_viewer() :
+    filters = list(dataset.dataset.view_generators.keys())
+    metas = list(dataset.dataset.data_generators.keys())
+    datasets = os.listdir(dataset.RAW_DIR)
+    text = ''
+
+    if (request.method == 'POST') :
+        dat = dataset.dataset(request.form['dataset'])
+        for v in request.form.getlist('filter') : #TODO add method to merge views right now will just show the last one
+            dat = dat.get_view(v)
+
+        result = dat.get_info(request.form['metas'])
+        text = str(result) #TODO method of displaying results
+
+    return render_template('viewer.html', filters=filters, metas = metas, datasets = datasets, text = text)
+
 
 @app.route('/streamer', methods=['GET', 'POST'])
 def streamer() :
